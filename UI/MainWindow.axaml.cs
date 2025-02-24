b3e9dbe7702fb80c6002565abc433426696bf67c
MarkYachnyy <markyachny@gmail.com>
2025-02-24 22:06:31 +0300

    Initial Made Queue UI in progress

---
+++ UI/MainWindow.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia.Controls;
+using IT_Task1.Classes;
+using System;
+using System.Collections;
+using System.Numerics;
+using Avalonia;
+
+namespace IT_Task1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public Point StartPoint
+    {
+        get
+        {
+            return new Point(0, 0);
+        }
+    }
+
+}
